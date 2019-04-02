bdcaba016bf0a0983da8eea28b22d0e381c62680
Laurence <laurencehall1@outlook.com>
2019-04-02 12:52:43 -0400

    removed public aws keys

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   JWT_SECRET: "shhhhhhhhhh",
   DB_URI:
     "mongodb://localhost:27017/scrapester",
-  AWS_ACCESS_KEY: "AKIAJ67DULWIU2Q4BEOA",
-  AWS_SECRET_KEY: "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"
+  // AWS_ACCESS_KEY: 
+  // AWS_SECRET_KEY: 
 };
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 if (process.env.NODE_ENV === "development") {
-  console.log(process.env.NODE_ENV);
   module.exports = require("./dev");
 } else {
   module.exports = require("./prod");
