6103756df433534dc0f275776d3bfec4e638537b
Laurence <laurencehall1@outlook.com>
2019-04-02 08:37:24 -0400

    added querystrings and fixed project structure, also added configuration for images to be saved to s3, not finished yet with image-config

--- controllers/RequestController.js
+++ controllers/RequestController.js
@@ -1,7 +1,52 @@
+const Requests = require("../requests/generalRequests");
 const Request = require("../models/request");
+const mongoose = require("mongoose");
+const { upload } = require("../image-config/index");
+const queryString = require("query-string");
+
 module.exports = {
+  async getText(req, res) {
+    let url = queryString.parse(req.url).url;
+    let element = queryString.parse(req.url).element;
+    let userId = queryString.parse(req.url).userId;
+
+    try {
+      let request = await Requests.getElementsText(url, element);
+      if (userId) {
+        // If user provides optional user id to save, then create new request and save it
+        let newRequest = new Request({
+          request: {
+            url,
+            query: element,
+            result: request
+          },
+          user: userId
+        });
+        const savedRequest = await newRequest.save();
+        // Send saved request
+        res.json(savedRequest);
+      } else {
+        // Send request without saving
+        res.json(request);
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  //TODO: Save to s3 and reference s3 bucket img url in mongo
+  async screenshot(req, res) {
+    let url = queryString.parse(req.url).url;
+    let filename = queryString.parse(req.url).filename;
+    try {
+      await Requests.screenshotPage(url, filename);
+      res.send("Screenshot has been saved");
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  },
   async savedRequests(req, res) {
-    const getSavedRequests = await Request.find({ user: req.params.id }).select('-user').select('-_id');
-    res.send(getSavedRequests);
+    let savedRequests = await Request.find({ user: req.params.id }).select('-_id');
+    res.send(savedRequests);
   }
 };
--- routes/requests.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const api = express.Router();
-const RequestController = require("../controllers/RequestController");
-
-api.get("/:id", RequestController.savedRequests);
-
-module.exports = api;
